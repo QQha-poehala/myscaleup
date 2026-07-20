0568b33b3251ed6992ef2ec647f91c81fa252357
Misha <myxa2003@gmail.com>
2026-07-20 20:00:04 +0700

    Add Yandex Metrika counter

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,6 +19,32 @@ export default defineNuxtConfig({
       htmlAttrs: { lang: 'ru' },
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1',
+       script: [
+        {
+          innerHTML: `
+            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();
+            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+            })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=110886707', 'ym');
+
+            ym(110886707, 'init', {
+              ssr:true, 
+              webvisor:true, 
+              clickmap:true, 
+              trackLinks:true, 
+              accurateTrackBounce:true
+            });
+          `,
+          type: 'text/javascript',
+        }
+      ],
+      // Добавляем noscript (на случай отключенного JS)
+      noscript: [
+        {
+          innerHTML: '<div><img src="https://mc.yandex.ru/watch/110886707" style="position:absolute; left:-9999px;" alt="" /></div>'
+        }
+      ]
     }
   }
 })
